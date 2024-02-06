69141153987920fedcb4f8338e12e91f4848026b
Rails Udemy Course <railsudemycourse@gmail.com>
2024-02-06 19:46:59 +0000

    json format

--- src/routes/admin/[slug]/+page.svelte
+++ src/routes/admin/[slug]/+page.svelte
@@ -8,10 +8,10 @@
       {#each hrdata['activities-heart'] as activity (activity.dateTime)}
         <div class="flex items-center space-x-4">
           <div class="flex">
-            <p class="font-bold">{activity.dateTime}</p>
+            <p class="">{activity.dateTime}</p>
           </div>
           {#if activity.value && activity.value.heartRateZones}
-            <div class="bg-gray-200 px-2 py-1 rounded overflow-x-scroll whitespace-nowrap font-mono leading-tight flex-grow">
+            <div class="bg-stone-200 px-2 py-1 rounded-sm overflow-x-scroll whitespace-nowrap leading-tight flex-grow">
               {#each activity.value.heartRateZones as zone (zone.name)}
                 Zone: {zone.name}, Max: {zone.max}, Min: {zone.min}{#if zone !== activity.value.heartRateZones[activity.value.heartRateZones.length - 1]}, {/if}
               {/each}
