52d55aa6bc1ab2ed7c8af1abcb2db784e0da7562
Rails Udemy Course <railsudemycourse@gmail.com>
2024-02-24 21:37:54 +0000

    /api/test

--- api/main.py
+++ api/main.py
@@ -2,6 +2,6 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/test")   # A simple GET endpoint
+@app.get("/api/test")   # A simple GET endpoint
 def hello_world():
     return {"message": "Hello from your FastAPI!"}
